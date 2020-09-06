e48031b62c6fcc1b94c448501c6029d0212b38e3
Zach <z61@zeesixtyone.com>
2020-09-06 17:09:57 -0400

    Remove user from home

--- public/js/app.js
+++ public/js/app.js
@@ -69996,13 +69996,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 var Home = function Home() {
-  var _useContext = Object(react__WEBPACK_IMPORTED_MODULE_0__["useContext"])(_context_user__WEBPACK_IMPORTED_MODULE_1__["default"]),
-      _useContext$userState = _useContext.userState,
-      userId = _useContext$userState.id,
-      avatar = _useContext$userState.avatar,
-      username = _useContext$userState.username;
-
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, "Home"), userId > 0 ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_partials_UserAvatar__WEBPACK_IMPORTED_MODULE_2__["default"], {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, userId > 0 ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_partials_UserAvatar__WEBPACK_IMPORTED_MODULE_2__["default"], {
     avatar: avatar
   }) : '');
 };
--- resources/js/components/Pages/Home.jsx
+++ resources/js/components/Pages/Home.jsx
@@ -3,12 +3,9 @@ import UserContext from "../../context/user";
 import UserAvatar from "../partials/UserAvatar";
 
 const Home = () => {
-    const {userState: {id: userId, avatar, username}} = useContext(UserContext)
     return (
         <>
-            <p>Home</p>
             { userId > 0 ? <UserAvatar avatar={avatar} /> : ''}
-
         </>
     )
 }
