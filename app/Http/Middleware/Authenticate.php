0077fb6228ee35ecb299853b8a278a0a589509d1
Zach <z61@zeesixtyone.com>
2020-11-10 12:41:32 -0500

    Merge API auth with normal auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Middleware;
 
+use App\User;
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    public function handle($request, Closure $next, ...$guards)
+    {
+        if ($request->hasHeader('X-API-KEY'))
+        {
+            $user = User::whereApiKey($request->header('X-API-KEY'))->first();
+
+            if ($user)
+            {
+                auth()->loginUsingId($user->id);
+
+                return $next($request);
+            }
+        }
+
+        return parent::handle($request, $next, ...$guards);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
